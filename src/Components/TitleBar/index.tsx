9e22a9dce57c49b270b0219caa6fe5b7f2d71196
wogha95 <wogha95@naver.com>
2023-03-08 00:08:40 +0900

    feat: TitleBar

---
+++ src/Components/TitleBar/index.tsx
@@ -0,0 +1,13 @@
+export default function TitleBar() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        lineHeight: "30px",
+        backgroundColor: "lightgray",
+      }}
+    >
+      Milliman
+    </div>
+  );
+}
