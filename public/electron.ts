361beec6de07b18fb4e76b7fd36a7c15910afa67
wogha95 <wogha95@naver.com>
2023-03-08 00:28:42 +0900

    refactor: erase unnecessary type

--- public/electron.ts
+++ public/electron.ts
@@ -1,14 +1,13 @@
 import * as path from "path";
 import * as url from "url";
-
 import { app, BrowserWindow } from "electron";
 import * as isDev from "electron-is-dev";
 
 const baseUrl = "http://localhost:3000";
 
-let mainWindow: BrowserWindow | null;
+let mainWindow: BrowserWindow | null = null;
 
-function createMainWindow(): void {
+function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 960,
     height: 630,
@@ -25,20 +24,20 @@ function createMainWindow(): void {
 
   mainWindow.loadURL(isDev ? baseUrl : mainWindowUrl);
 
-  mainWindow.on("closed", (): void => {
+  mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
-app.on("ready", (): void => {
+app.on("ready", () => {
   createMainWindow();
 });
 
-app.on("window-all-closed", (): void => {
+app.on("window-all-closed", () => {
   app.quit();
 });
 
-app.on("activate", (): void => {
+app.on("activate", () => {
   if (mainWindow === null) {
     createMainWindow();
   }
