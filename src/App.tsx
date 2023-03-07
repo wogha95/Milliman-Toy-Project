76e6d998e1c03c4511e3620f1fd61b7ea1826538
wogha95 <wogha95@naver.com>
2023-03-08 00:10:16 +0900

    feat: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import TitleBar from "./Components/TitleBar";
+import SideBar from "./Components/SideBar";
+import Main from "./Components/Main";
+
 export default function App() {
-  return <div className="App">App Component</div>;
+  return (
+    <div style={{ width: 960, height: 600 }}>
+      <header style={{ width: "100%", height: 30 }}>
+        <TitleBar />
+      </header>
+      <main style={{ display: "flex", width: "100%", height: "100%" }}>
+        <aside style={{ width: 230, height: "100%", overflowY: "auto" }}>
+          <SideBar />
+        </aside>
+        <main>
+          <Main />
+        </main>
+      </main>
+    </div>
+  );
 }
